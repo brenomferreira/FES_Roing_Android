a3a45b83e6288fca78a0bd9b19a0cd9099da581d
brenomferreira <brenomferreira@hotmail.com>
2019-05-02 17:24:29 -0300

    Inicio funçoes dos botoes

--- app/src/main/java/com/example/fes_roing_android/MainActivity.java
+++ app/src/main/java/com/example/fes_roing_android/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.fes_roing_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.conecta_bluetooth = (Button) findViewById(R.id.btn_conectBTh);
+        this.mViewHolder.parametros = (Button) findViewById(R.id.btn_parametros);
+
+        this.mViewHolder.parametros.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btn_parametros){
+            // Lógica de navegação.
+            // Intent intent = new Intent(getApplicationContext(), ParametrosActivity.class);
+            Intent intent = new Intent (this, ParametrosActivity.class);
+            startActivity(intent);
+        }
+        else if  (id == R.id.btn_conectBTh){
+            // logica da conexão
+
+        }
+
+    }
+
+
+    private static class ViewHolder{
+        Button conecta_bluetooth;
+        Button parametros;
+    }
+
+
+
+
+
 }
+
+
+
