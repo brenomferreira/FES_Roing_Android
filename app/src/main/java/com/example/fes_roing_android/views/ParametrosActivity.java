c638f793a60de90ecbf1295d3ac306c728cf97dc
brenomferreira <brenomferreira@hotmail.com>
2019-05-07 15:53:37 -0300

    implementado movimento do seekBar a partir do EditText

--- app/src/main/java/com/example/fes_roing_android/views/ParametrosActivity.java
+++ app/src/main/java/com/example/fes_roing_android/views/ParametrosActivity.java
@@ -8,57 +8,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.example.fes_roing_android.constantes.ParametrosConstantes;
 import com.example.fes_roing_android.R;
+import com.example.fes_roing_android.constantes.ParametrosConstantes;
 import com.example.fes_roing_android.util.SecurityPreferences;
 
-import org.w3c.dom.Text;
-
 public class ParametrosActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
-    
+
     private ViewHolder mViewHolder = new ViewHolder();
     private SecurityPreferences mSecurityPreferences;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_parametros);
 
-
-
-
-        // util packages
+// util packages
         this.mSecurityPreferences = new SecurityPreferences(this);
 
-        // TextViews
+//SeekBars
+        mViewHolder.seekBar_CH12 = (SeekBar) findViewById(R.id.seekBar_CH12);
+
+// TextViews
         mViewHolder.valor_previo = (TextView) findViewById(R.id.textView_valor_previo);
 
-        // OnClick
+// OnClick
         this.mViewHolder.btn_voltar = (Button) findViewById(R.id.btn_voltar_ac_Parametros);
         this.mViewHolder.btn_voltar.setOnClickListener(this);
         this.mViewHolder.check_CH12 = (CheckBox) findViewById(R.id.checkboxCH12);
         this.mViewHolder.check_CH12.setOnClickListener(this);
 
-        // OnKey
+// OnKey
         this.mViewHolder.editText_CH12 = (EditText) findViewById(R.id.editText_CH12);
         this.mViewHolder.editText_CH12.setOnKeyListener(this);
-    
+
     } // Fim onCreate
-    
+
     @Override
     public void onClick(View view) {
         int id = view.getId();
         if (id == R.id.checkboxCH12) {
             if (this.mViewHolder.check_CH12.isChecked()) {
-                // True
+// True
                 this.mSecurityPreferences.storeString(ParametrosConstantes.AtiveCH12, ParametrosConstantes.CONFIRMADO_CH); // (Key, valor)
 
             } // Fim se check true
             else {
-                // False
+// False
                 this.mSecurityPreferences.storeString(ParametrosConstantes.AtiveCH12, ParametrosConstantes.NAO_CONFIRMADO_CH); // (Key, valor)
 
             } // Fim se check false
@@ -69,7 +68,7 @@ public class ParametrosActivity extends AppCompatActivity implements View.OnClic
             startActivity(intent2);
 
         } // Fim lógica btn_voltar_ac_Parametros
-    
+
     } // Fim onClick
 
     @Override
@@ -77,9 +76,12 @@ public class ParametrosActivity extends AppCompatActivity implements View.OnClic
         int id = view.getId();
         int evento_key = event.getKeyCode();
 
-        if (id == R.id.editText_CH12){
-            if(evento_key == 66 ){ // 66 corresponde à tecla ENTER
+        if (id == R.id.editText_CH12) {
+            if (evento_key == 66) { // 66 corresponde à tecla ENTER
                 this.mViewHolder.valor_previo.setText("teste");
+                int value = (int) Integer.parseInt(String.valueOf(this.mViewHolder.editText_CH12.getText()));
+                this.mViewHolder.seekBar_CH12.setProgress(value);
+
             }
 
         } // Fim dos eventos teclados ch12
@@ -88,14 +90,15 @@ public class ParametrosActivity extends AppCompatActivity implements View.OnClic
     }// Fim Método onKey
 
     private static class ViewHolder {
-        Button btn_voltar;
-        CheckBox check_CH12;
-        CheckBox check_CH34;
-        CheckBox check_CH56;
-        CheckBox check_CH78;
-        EditText editText_CH12;
-        TextView valor_previo;
+        Button      btn_voltar;
+        CheckBox    check_CH12;
+        CheckBox    check_CH34;
+        CheckBox    check_CH56;
+        CheckBox    check_CH78;
+        EditText    editText_CH12;
+        SeekBar     seekBar_CH12;
+        TextView    valor_previo;
 
     }// Fim class ViewHolder
-    
+
 }
