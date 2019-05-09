23b80b20b1ad2197ef3ce62b02dfa56143dfa74f
brenomferreira <brenomferreira@hotmail.com>
2019-05-09 15:50:24 -0300

    todas aos paramentros (layout) na activity cadencia

--- app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
+++ app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
@@ -3,7 +3,10 @@ package com.example.fes_roing_android.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.fes_roing_android.R;
+
 public class CadenciaActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
