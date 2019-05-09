93d9fc4a014af9e8baf46603795f94f8613f1a2e
brenomferreira <brenomferreira@hotmail.com>
2019-05-09 10:33:59 -0300

    relative layout

---
+++ app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
@@ -0,0 +1,13 @@
+package com.example.fes_roing_android.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadenciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadencia);
+    }
+}
