61dbc8f94fc2f7a60205c3fd9b26ea8a2aa2d54d
brenomferreira <brenomferreira@hotmail.com>
2019-05-10 17:51:54 -0300

    inserido botoes de treino e calculo de voga e espaçamento do layout cadencia + campo para escrever os senos

--- app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
+++ app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
@@ -2,15 +2,59 @@ package com.example.fes_roing_android.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.CheckBox;
 
 import com.example.fes_roing_android.R;
 
-public class CadenciaActivity extends AppCompatActivity {
+public class CadenciaActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadencia);
+
+
+
+        // Onclick
+        this.mViewHolder.checkBox_Voga = (CheckBox) findViewById(R.id.checkBox_Voga);
+        this.mViewHolder.checkBox_Voga.setOnClickListener(this);
+        this.mViewHolder.checkBox_Drive = (CheckBox) findViewById(R.id.checkBox_Drive);
+        this.mViewHolder.checkBox_Drive.setOnClickListener(this);
+
+
+
+
     }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.checkBox_Voga){
+
+
+
+        }
+
+    }
+
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent event) {
+        return false;
+    }
+
+
+    private static class ViewHolder {
+
+        CheckBox checkBox_Voga;
+        CheckBox checkBox_Drive;
+
+
+
+    }
+
 }
