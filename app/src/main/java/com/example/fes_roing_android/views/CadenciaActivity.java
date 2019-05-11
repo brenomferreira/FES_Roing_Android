956b1275bffa4272308a284ef08acf00952d4bf1
brenomferreira <brenomferreira@hotmail.com>
2019-05-11 14:24:04 -0300

    Lógica dos checkbox dependentes entre voga e drive layout aqueciemento

--- app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
+++ app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import com.example.fes_roing_android.R;
 
@@ -18,14 +20,17 @@ public class CadenciaActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_cadencia);
 
 
-
         // Onclick
         this.mViewHolder.checkBox_Voga = (CheckBox) findViewById(R.id.checkBox_Voga);
         this.mViewHolder.checkBox_Voga.setOnClickListener(this);
         this.mViewHolder.checkBox_Drive = (CheckBox) findViewById(R.id.checkBox_Drive);
         this.mViewHolder.checkBox_Drive.setOnClickListener(this);
 
-
+        //Areas layout
+        this.mViewHolder.area_configDrive = (LinearLayout) findViewById(R.id.area_config_Drive);
+        this.mViewHolder.area_configVoga = (LinearLayout) findViewById(R.id.area_config_Voga);
+        this.mViewHolder.area_radioButton = (LinearLayout) findViewById(R.id.area_radioButton);
+        setEnableViews(this.mViewHolder.area_radioButton, false);
 
 
     }
@@ -34,11 +39,31 @@ public class CadenciaActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         int id = view.getId();
 
-        if (id == R.id.checkBox_Voga){
+        if (id == R.id.checkBox_Voga) {
+            if (this.mViewHolder.checkBox_Voga.isChecked()) {
+                setEnableViews(this.mViewHolder.area_configDrive, false);
+                setEnableViews(this.mViewHolder.area_radioButton, true);
+                this.mViewHolder.checkBox_Drive.setChecked(false);
+            } else {
+                setEnableViews(this.mViewHolder.area_configDrive, true);
+                if (!mViewHolder.checkBox_Drive.isChecked()) {
+                    setEnableViews(this.mViewHolder.area_radioButton, false);
+                }
+            }
+        }// Fim lógica CheckBox Voga
 
+        if (id == R.id.checkBox_Drive) {
+            if (this.mViewHolder.checkBox_Drive.isChecked()) {
+                setEnableViews(this.mViewHolder.area_configVoga, false);
+                setEnableViews(this.mViewHolder.area_radioButton, true);
+                this.mViewHolder.checkBox_Voga.setChecked(false);
 
-
-        }
+            } else {
+                setEnableViews(this.mViewHolder.area_configVoga, true);
+                if (!mViewHolder.checkBox_Voga.isChecked()){
+                setEnableViews(this.mViewHolder.area_radioButton, false);}
+            }
+        }// Fim lógica CheckBox Drive
 
     }
 
@@ -53,8 +78,25 @@ public class CadenciaActivity extends AppCompatActivity implements View.OnClickL
         CheckBox checkBox_Voga;
         CheckBox checkBox_Drive;
 
+        //Areas layout
+        LinearLayout area_configDrive;
+        LinearLayout area_configVoga;
+        LinearLayout area_radioButton;
 
 
     }
+
+
+    public void setEnableViews(View v, boolean enable) {
+        v.setEnabled(enable);
+        if (v instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) v).getChildCount(); i++) {
+                View view = ((ViewGroup) v).getChildAt(i);
+                setEnableViews(view, enable);
+            }
+        }
+    }
+
 
 }
