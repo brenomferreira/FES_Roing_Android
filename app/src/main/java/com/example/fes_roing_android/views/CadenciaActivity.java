53855ee91f9ac1972466d5acdba3771a5ecd352b
brenomferreira <brenomferreira@hotmail.com>
2019-05-13 17:12:38 -0300

    salvando segunda feira para continuar em casa,,, problemas ainda no checkbox voga e drive

--- app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
+++ app/src/main/java/com/example/fes_roing_android/views/CadenciaActivity.java
@@ -1,8 +1,7 @@
 package com.example.fes_roing_android.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +9,12 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.fes_roing_android.R;
-import com.example.fes_roing_android.constantes.ParametrosConstantes;
 import com.example.fes_roing_android.util.SecurityPreferences;
 
-public class CadenciaActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
+public class CadenciaActivity extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
     private SecurityPreferences mSecurityPreferences;
@@ -26,29 +25,28 @@ public class CadenciaActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_cadencia);
 
 
-
         // Onclick
         this.mViewHolder.checkBox_Voga = (CheckBox) findViewById(R.id.checkBox_Voga);
         this.mViewHolder.checkBox_Voga.setOnClickListener(this);
         this.mViewHolder.checkBox_Drive = (CheckBox) findViewById(R.id.checkBox_Drive);
         this.mViewHolder.checkBox_Drive.setOnClickListener(this);
 
-        // OnKey
+
+        // onEditorAction
         this.mViewHolder.editText_Cadeira = (EditText) findViewById(R.id.editText_Cadeira);
-        this.mViewHolder.editText_Cadeira.setOnKeyListener(this);
+        this.mViewHolder.editText_Cadeira.setOnEditorActionListener(this);
         this.mViewHolder.editText_Drive = (EditText) findViewById(R.id.editText_Drive);
-        this.mViewHolder.editText_Drive.setOnKeyListener(this);
+        this.mViewHolder.editText_Drive.setOnEditorActionListener(this);
         this.mViewHolder.editText_Freq = (EditText) findViewById(R.id.editText_Freq);
-        this.mViewHolder.editText_Freq.setOnKeyListener(this);
+        this.mViewHolder.editText_Freq.setOnEditorActionListener(this);
         this.mViewHolder.editText_Voga = (EditText) findViewById(R.id.editText_Voga);
-        this.mViewHolder.editText_Voga.setOnKeyListener(this);
+        this.mViewHolder.editText_Voga.setOnEditorActionListener(this);
 
         //Areas layout
         this.mViewHolder.area_configDrive = (LinearLayout) findViewById(R.id.area_config_Drive);
         this.mViewHolder.area_configVoga = (LinearLayout) findViewById(R.id.area_config_Voga);
         this.mViewHolder.area_radioButton = (LinearLayout) findViewById(R.id.area_radioButton);
         setEnableViews(this.mViewHolder.area_radioButton, false);
-
 
 
         // habilitar botao done
@@ -68,53 +66,37 @@ public class CadenciaActivity extends AppCompatActivity implements View.OnClickL
             if (this.mViewHolder.checkBox_Voga.isChecked()) {
                 setEnableViews(this.mViewHolder.area_configDrive, false);
                 setEnableViews(this.mViewHolder.area_radioButton, true);
-                this.mViewHolder.checkBox_Drive.setChecked(false);
+                if (this.mViewHolder.checkBox_Drive.isChecked()) {
+                    this.mViewHolder.checkBox_Drive.setChecked(false);
+                    setEnableViews(this.mViewHolder.area_configDrive, false);
+                }
+
+
             } else {
                 setEnableViews(this.mViewHolder.area_configDrive, true);
+
                 if (!mViewHolder.checkBox_Drive.isChecked()) {
                     setEnableViews(this.mViewHolder.area_radioButton, false);
                 }
             }
         }// Fim lógica CheckBox Voga
 
-        if (id == R.id.checkBox_Drive) {
-            if (this.mViewHolder.checkBox_Drive.isChecked()) {
-                setEnableViews(this.mViewHolder.area_configVoga, false);
-                setEnableViews(this.mViewHolder.area_radioButton, true);
-                this.mViewHolder.checkBox_Voga.setChecked(false);
-
-            } else {
-                setEnableViews(this.mViewHolder.area_configVoga, true);
-                if (!mViewHolder.checkBox_Voga.isChecked()){
-                setEnableViews(this.mViewHolder.area_radioButton, false);}
-            }
-        }// Fim lógica CheckBox Drive
 
     }
 
     @Override
-    public boolean onKey(View view, int keyCode, KeyEvent event) {
+    public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
 
         int id = view.getId();
 
-        if (id == R.id.editText_Voga){
-           if (keyCode == 66){
-               String text = this.mViewHolder.editText_Voga.getText().toString();
-               this.mSecurityPreferences.storeString(ParametrosConstantes.valorVoga, text);
-               //calcularParametros();
-
-           }
-
+        if (id == R.id.editText_Voga) {
+            if (event == null) {
+                calcularParametros();
+            }
         }
 
 
-
-
-
-
-
         return false;
-
     }
 
 
@@ -134,20 +116,19 @@ public class CadenciaActivity extends AppCompatActivity implements View.OnClickL
         LinearLayout area_radioButton;
 
 
-
     }
 
 
-    public void calcularParametros(){
+    public void calcularParametros() {
 
-        if (this.mViewHolder.checkBox_Voga.isChecked()){
+        if (this.mViewHolder.checkBox_Voga.isChecked()) {
 
 
-            this.mViewHolder.editText_Drive.setText("999");
+            this.mViewHolder.editText_Freq.setText("999");
         }
-        if (this.mViewHolder.checkBox_Drive.isChecked()){
+        if (this.mViewHolder.checkBox_Drive.isChecked()) {
 
-            this.mViewHolder.editText_Voga.setText("999");
+            this.mViewHolder.editText_Cadeira.setText("888");
 
         }
     }
